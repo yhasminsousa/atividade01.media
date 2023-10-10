c2b0ce415977e379b65bb668e4a631480c072396
palomalindinha01 <palomalindinha35@gmail.com>
2023-10-10 16:50:30 -0300

    adicionado recurso de imput

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # atividade01.media
+nota01 = 8
+nota02 = 9
+nota03 = 6
+media = (nota01 + nota02+ nota03)/3
+print (sua meédia é:", media)
--- questao01.py
+++ questao01.py
@@ -1,5 +1,5 @@
-nota01 = 10
-nota02 = 8
-nota03 = 6
+nota01 = float(input("digite a nota01"))
+nota02 = float(input("digite a nota02"))
+nota03 = float(input("digite a nota03"))
 media = (nota01 + nota02 + nota03) / 3
 print('sua média é:', media)
